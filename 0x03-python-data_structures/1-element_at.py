321d9a6d28cf5f0de2d38c24209ab73fecdb6ab5
lloydkioko <kiokolloyd@gmail.com>
2023-01-07 04:28:45 -0800

    data structures

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -4,6 +4,6 @@ def element_at(my_list, idx):
     if idx < 0 or idx >= len(my_list):
         return
     else:
-        forx in range(0, len(my_list)):
+        for x in range(0, len(my_list)):
         if x = idx:
             return(my_list[idx])
