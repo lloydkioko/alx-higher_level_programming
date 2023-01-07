b82d119aaa357250fafcb76b8e94abaae09380d2
lloydkioko <kiokolloyd@gmail.com>
2023-01-07 04:48:58 -0800

    data structures

---
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+
+def replace_in_list(my_list, idx, element):
+    if idx < 0 or idx >= len(my_list):
+        return
+    else:
+        for x in range(0, len(my_list)):
+            if x == idx:
+                return my_list
