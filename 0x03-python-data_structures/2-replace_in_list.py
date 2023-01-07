cdf601b228de89214e2793980e36c1d4e0188a45
lloydkioko <kiokolloyd@gmail.com>
2023-01-07 04:57:29 -0800

    data structures

--- 0x03-python-data_structures/2-replace_in_list.py
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -2,7 +2,7 @@
 
 def replace_in_list(my_list, idx, element):
     if idx < 0 or idx >= len(my_list):
-        return
+        return my_list
     else:
         for x in range(0, len(my_list)):
             if x == idx:
