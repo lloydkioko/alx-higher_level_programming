639b9e0bc2aaf3a0d561db76bcb0751fce6460c1
lloydkioko <kiokolloyd@gmail.com>
2023-01-09 02:41:01 -0800

    data structures

---
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+def new_in_list(my_list, idx, element):
+    list_copy = my_list.copy()
+    if idx < 0 or idx >= len(list_copy):
+        return list_copy
+    else:
+        for x in range(0, len(list_copy)):
+            if x == idx:
+                list_copy[idx] = element
+                return list_copy
+        
