c0982fbb588ebbef4254c42340c956571ce9dc51
lloydkioko <kiokolloyd@gmail.com>
2023-02-07 13:39:50 -0800

    update

--- 0x0B-python-input_output/10-student.py
+++ 0x0B-python-input_output/10-student.py
@@ -1,24 +1,24 @@
 #!/usr/bin/python3
-"""Contains the clas "Student"""
+""" Student Class Module """
 
 
 class Student:
-    """Representation of a student"""
+    """ Student Class """
+
     def __init__(self, first_name, last_name, age):
-        """Initializes the student"""
+        """ Initializes the class """
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
 
     def to_json(self, attrs=None):
-        """returns a dictionary representation of a Student instance
-        with specified attributes"""
-        if attrs is None:
+        """ Function returns the dictionary
+        description of a Student instance """
+        if type(attrs) is list and all(type(i) is str for i in attrs):
+            attr_dict = {}
+            for elem in attrs:
+                if hasattr(self, elem):
+                    attr_dict[elem] = self.__dict__[elem]
+            return attr_dict
+        else:
             return self.__dict__
-        new_dict = {}
-        for a in attrs:
-            try:
-                new_dict[a] = self.__dict__[a]
-            except:
-                pass
-        return new_dict
