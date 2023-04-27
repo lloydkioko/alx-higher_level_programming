8c1f371fa20078a75722e0f1311b17c8ac28d642
lloyd kioko <ch.abene254@gmail.com>
2023-04-28 01:36:19 +0300

    initial

--- 0x10-python-network_0/6-main.py
+++ 0x10-python-network_0/6-main.py
@@ -1,4 +1,3 @@
-
 #!/usr/bin/python3
 """ Test function find_peak """
 find_peak = __import__('6-peak').find_peak
--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -1,16 +1,30 @@
 #!/usr/bin/python3
-'''
-module to find peak
-'''
+"""module to find a peak in a list of unsorted integers"""
 
 
-def find_peak(listint):
-    ''' Function that returns peak value in a list '''
-    if listint:
-        m = listint[0]
-        for i in listint:
-            if i > m:
-                m = i
-        return m
-    else:
+def find_peak(list_of_integers):
+    """
+    Args:
+        list_of_integers(int): list of integers to find peak of
+    Returns: peak of list_of_integers or None
+    """
+    size = len(list_of_integers)
+    mid_e = size
+    mid = size // 2
+
+    if size == 0:
         return None
+
+    while True:
+        mid_e = mid_e // 2
+        if (mid < size - 1 and
+                list_of_integers[mid] < list_of_integers[mid + 1]):
+            if mid_e // 2 == 0:
+                mid_e = 2
+            mid = mid + mid_e // 2
+        elif mid_e > 0 and list_of_integers[mid] < list_of_integers[mid - 1]:
+            if mid_e // 2 == 0:
+                mid_e = 2
+            mid = mid - mid_e // 2
+        else:
+            return list_of_integers[mid]
